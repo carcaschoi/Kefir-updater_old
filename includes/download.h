fbb6630aed57f11313a8b41528abc0cade5b86b8
carcaschoi <64573431+carcaschoi@users.noreply.github.com>
2021-05-31 20:28:26 +0800

    Update download.h

--- includes/download.h
+++ includes/download.h
@@ -1,9 +1,9 @@
 #ifndef _DOWNLOAD_H_
 #define _DOWNLOAD_H_
 
-#define KEF_URL    	 		"https://api.github.com/repos/rashevskyv/kefir/releases"
-#define KEFIR_URL 	 		"https://github.com/rashevskyv/kefir/releases/latest/download/kefir.zip"
-#define APP_URL   	 	 	"https://github.com/rashevskyv/kefirupdater/releases/latest/download/kefirupdater.nro"
+#define KEF_URL    	 		"https://api.github.com/repos/carcaschoi/ShallowSea/releases"
+#define KEFIR_URL 	 		"https://github.com/carcaschoi/ShallowSea/releases/latest/download/ShallowSea-ams.zip"
+#define APP_URL   	 	 	"https://github.com/carcaschoi/ShallowSea/releases/latest/download/ShallowSea-updater.nro"
 
 
 int downloadFile(const char *url, const char *output, int api_mode);
